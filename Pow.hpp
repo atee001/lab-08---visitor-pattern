57183a83eb5017e6709ffb34a51995185ad0ecf3
CJT578078 <CameronJustinTsai@gmail.com>
2021-05-23 17:25:07 -0700

    Add Sub Pow

---
+++ Pow.hpp
@@ -0,0 +1,43 @@
+#pragma once 
+#include "base.hpp"
+
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+	class Pow: public Base {
+		public:
+			Pow(Base* x, Base* y): Base() {
+
+				lhs = x;
+				rhs = y;
+
+			}
+			
+			~Pow(){
+
+				delete lhs;
+				delete rhs;
+
+			}
+
+			double evaluate() {
+				
+
+				return pow(lhs->evaluate(), rhs->evaluate());
+			}
+
+			string stringify() {
+				return "(" + lhs->stringify() + "**" + rhs->stringify() + ")";
+			}
+
+
+		private:
+
+			Base* lhs;
+			Base* rhs;
+
+
+
+	};

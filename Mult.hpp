245b6f334a407f7f40a22c371f00efecc81306bd
atee001 <80326381+atee001@users.noreply.github.com>
2021-05-25 00:14:51 -0700

    Update Mult.hpp

--- Mult.hpp
+++ Mult.hpp
@@ -4,50 +4,50 @@
 
 class Mult : public Base {
 
-  public:
+	public:
 
-    Mult(Base* lhs, Base* rhs) : Base(){
+		Mult(Base* lhs, Base* rhs) : Base(){
 
-      this->lhs = lhs;
-      this->rhs = rhs;
+			this->lhs = lhs;
+			this->rhs = rhs;
 
-    }
-	
-    ~Mult(){
-	delete lhs;
-	delete rhs;
-    }
+		}
 
-    double evaluate(){
+		~Mult(){
+			delete lhs;
+			delete rhs;
+		}
 
-      return lhs->evaluate()*rhs->evaluate();
+		double evaluate(){
 
-    }
+			return lhs->evaluate()*rhs->evaluate();
 
-    string stringify(){
+		}
 
-      return "(" + lhs->stringify() + "*" + rhs->stringify() + ")";
+		string stringify(){
 
-    }
-    Base* get_child(int i){
-      
-      return (i == 0) ? lhs : (i == 1) ? rhs : nullptr;
+			return "(" + lhs->stringify() + "*" + rhs->stringify() + ")";
 
-    }
+		}
+		Base* get_child(int i){
 
-    int number_of_children() {
-             return 2;
-     }
+			return (i == 0) ? lhs : (i == 1) ? rhs : nullptr;
 
-    void accept(Visitor* visitor, int index){
+		}
 
-	return (index == 0) ? visitor->visit_mult_begin(this) : (index == 1) ? visitor->visit_mult_middle(this) : visitor->visit_mult_end(this);
+		int number_of_children() {
+			return 2;
+		}
 
-    }
+		void accept(Visitor* visitor, int index){
 
-  private:
+			return (index == 0) ? visitor->visit_mult_begin(this) : (index == 1) ? visitor->visit_mult_middle(this) : visitor->visit_mult_end(this);
 
-    Base* lhs;
-    Base* rhs;
+		}
+
+	private:
+
+		Base* lhs;
+		Base* rhs;
 
 };

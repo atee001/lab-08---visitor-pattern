a40d4a9dfceb4c1e49ec4a5aee36430f1c9527b7
atee001 <80326381+atee001@users.noreply.github.com>
2021-05-25 00:15:28 -0700

    Update Pow.hpp

--- Pow.hpp
+++ Pow.hpp
@@ -6,54 +6,54 @@
 
 using namespace std;
 
-	class Pow: public Base {
-		public:
-			Pow(Base* x, Base* y): Base() {
+class Pow: public Base {
+	public:
+		Pow(Base* x, Base* y): Base() {
 
-				lhs = x;
-				rhs = y;
+			lhs = x;
+			rhs = y;
 
-			}
-			
-			~Pow(){
+		}
 
-				delete lhs;
-				delete rhs;
+		~Pow(){
 
-			}
+			delete lhs;
+			delete rhs;
 
-			double evaluate() {
-				
+		}
 
-				return pow(lhs->evaluate(), rhs->evaluate());
-			}
-
-			string stringify() {
-				return "(" + lhs->stringify() + "**" + rhs->stringify() + ")";
-			}
-			Base* get_child(int i){
-        
-        return (i == 0) ? lhs : (i == 1) ? rhs : nullptr;
-
-			}
-    
-			int number_of_children() {
-				return 2;
-			}
-		
-			 void accept(Visitor* visitor, int index){
-
-        			return (index == 0) ? visitor->visit_pow_begin(this) : (index == 1) ? visitor->visit_pow_middle(this) : visitor->visit_pow_end(this);
-
-    			}	
+		double evaluate() {
 
 
+			return pow(lhs->evaluate(), rhs->evaluate());
+		}
 
-		private:
+		string stringify() {
+			return "(" + lhs->stringify() + "**" + rhs->stringify() + ")";
+		}
+		Base* get_child(int i){
 
-			Base* lhs;
-			Base* rhs;
+			return (i == 0) ? lhs : (i == 1) ? rhs : nullptr;
+
+		}
+
+		int number_of_children() {
+			return 2;
+		}
+
+		void accept(Visitor* visitor, int index){
+
+			return (index == 0) ? visitor->visit_pow_begin(this) : (index == 1) ? visitor->visit_pow_middle(this) : visitor->visit_pow_end(this);
+
+		}	
 
 
 
-	};
+	private:
+
+		Base* lhs;
+		Base* rhs;
+
+
+
+};
